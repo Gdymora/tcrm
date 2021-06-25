dd1a9b60d99d71cc21257ef40aab82c5ae65dbcf
Alexandr <alekspetrofint@gmail.com>
2021-06-25 17:05:57 +0300

    fix, create click js menu, create report

--- js/lib.js
+++ js/lib.js
@@ -8,18 +8,55 @@ let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+if (btn) {
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
     modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+/*TAB */
+function openTab(evt, tabName) {
+    // Declare all variables
+    let i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+/**nav menu */
+
+let hid = document.getElementById('hider');
+if (hid) {
+    hid.onclick = function () {
+        var el = document.querySelector('.nav-links');
+        console.log(44)
+        el.style.display === 'none' ? el.style.display = 'block' : el.style.display = 'none';
+
+    }
+}
+
